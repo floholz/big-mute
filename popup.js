77c599e31c3382bed8df71adcdbb84cf2ad3da80
floholz <holzmann96flo@gmail.com>
2023-09-08 01:59:09 +0200

    try mute chrome tabs api

--- popup.js
+++ popup.js
@@ -1,3 +1,25 @@
-document.getElementById('popup-btn').addEventListener('click', () => {
-    window.alert('clicked!');
+document.getElementById('popup-btn').addEventListener('click', async () => {
+    // window.alert('clicked!');
+
+
+    const currentTab = await getCurrentTab();
+    console.log(currentTab);
+
+    await toggleMuteState(currentTab.id);
+
 });
+
+
+async function toggleMuteState(tabId) {
+    const tab = await chrome.tabs.get(tabId);
+    const muted = !tab.mutedInfo.muted;
+    await chrome.tabs.update(tabId, {muted});
+    console.log(`Tab ${tab.id} is ${muted ? "muted" : "unmuted"}`);
+}
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
