5a72e2042ec2f1cc56b7b8810786ba8d65a1af51
floholz <holzmann96flo@gmail.com>
2023-09-24 02:47:17 +0200

    > added minimize button // todo: save states, position // todo: add pause

--- src/content/content.js
+++ src/content/content.js
@@ -15,14 +15,72 @@ matSymLinkElement.rel = 'stylesheet';
 matSymLinkElement.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0';
 document.head.append(matSymLinkElement);
 
+let minimized = false;
+const bigMuteMinimize = document.createElement('button');
+bigMuteMinimize.id = 'bigMute_minimize';
+bigMuteMinimize.classList.add('material-symbols-outlined');
+bigMuteMinimize.innerText = 'unfold_less';
+bigMuteMinimize.onmouseup = (e) =>  {
+    if (e.button === 2) return; // ignore mouse2 events
+    if (clickIsHandledByDrag) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        return;
+    }
+    console.log('bigMuteMinimize clicked');
+    minimized = !minimized;
+    bigMuteMinimize.innerText = minimized ? 'unfold_more' : 'unfold_less';
+
+    if (minimized) {
+        bigMuteButton.classList.add('minimized');
+        bigMuteContent.classList.add('minimized');
+        bigMuteMinimize.classList.add('minimized');
+    } else {
+        bigMuteButton.classList.remove('minimized');
+        bigMuteContent.classList.remove('minimized');
+        bigMuteMinimize.classList.remove('minimized');
+    }
+
+    e.preventDefault();
+    e.stopImmediatePropagation();
+};
+
+const bigMuteContent = document.createElement('span');
+bigMuteContent.id = 'bigMute_content';
+bigMuteContent.innerText = 'volume_up';
+bigMuteContent.onmouseup = (e) => {
+    if (e.button === 2) return; // ignore mouse2 events
+    if (clickIsHandledByDrag) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    }
+};
+
 const bigMuteButton = document.createElement('button');
 bigMuteButton.id = 'bigMute_button';
 bigMuteButton.classList.add('material-symbols-outlined');
-bigMuteButton.innerText = 'volume_up';
+bigMuteButton.onmouseup = (e) => {
+    if (e.button === 2) return; // ignore mouse2 events
+    if (clickIsHandledByDrag) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        return;
+    }
+    console.log('bigMuteButton clicked');
+    toggleMuteState();
+    e.preventDefault();
+    e.stopImmediatePropagation();
+}
+bigMuteButton.append(bigMuteContent);
+
+const bigMuteMainElement = document.createElement('div');
+bigMuteMainElement.id = 'bigMute_main';
+bigMuteMainElement.append(bigMuteButton);
+bigMuteMainElement.append(bigMuteMinimize);
 
 const bigMuteContainer = document.createElement('div');
 bigMuteContainer.id = 'bigMute_container';
-bigMuteContainer.append(bigMuteButton);
+bigMuteContainer.append(bigMuteMainElement);
 
 document.addEventListener('fullscreenchange', () => {
     placeContainerInRootElement();
@@ -38,17 +96,17 @@ let timer;
 document.addEventListener('mousemove', function() {
     if (timer) return;
 
-    bigMuteButton.classList.add('moving');
+    bigMuteMainElement.classList.add('moving');
     clearTimeout(timer)
     timer = setTimeout(() => {
-        bigMuteButton.classList.remove('moving')
+        bigMuteMainElement.classList.remove('moving')
         timer = null;
     }, 500);
 });
 
 getCurrentMuteState().then(setMuteButtonContent);
 placeContainerInRootElement();
-dragElement(bigMuteButton);
+dragElement(bigMuteMainElement, bigMuteContainer);
 
 
 function placeContainerInRootElement() {
@@ -60,19 +118,26 @@ function placeContainerInRootElement() {
     }
 }
 
-function dragElement(element) {
+let clickIsHandledByDrag = false;
+function dragElement(element, container) {
     let initPosX = 0, initPosY = 0,
         nextPosX = 0, nextPosY = 0,
         prevPosX = 0, prevPosY = 0;
     let mouseDragged = false;
+    let target = element;
     element.onmousedown = dragMouseDown;
 
     function dragMouseDown(e) {
         e = e || window.event;
         e.preventDefault();
+        e.stopImmediatePropagation();
 
         // only listen for mouse1 events
         if (e.buttons !== 1) return;
+        target = e.target;
+
+        console.log(e.target.id, e.type);
+        clickIsHandledByDrag = true;
 
         // get the mouse cursor position at startup:
         initPosX = prevPosX = e.clientX;
@@ -85,10 +150,12 @@ function dragElement(element) {
     function elementDrag(e) {
         e = e || window.event;
         e.preventDefault();
+        e.stopImmediatePropagation();
+        console.log(e.target.id, e.type);
         mouseDragged = true;
 
-        const dragArea = bigMuteContainer.getClientRects()[0];
-        const dragHandle = bigMuteButton.getClientRects()[0];
+        const dragArea = container.getClientRects()[0];
+        const dragHandle = element.getClientRects()[0];
 
         // calculate the new cursor position:
         const changeX = prevPosX - e.clientX;
@@ -117,33 +184,36 @@ function dragElement(element) {
         element.style.left = left + "px";
     }
 
-    function closeDragElement() {
+    function closeDragElement(e) {
         const dist = Math.sqrt(Math.pow(initPosX - prevPosX, 2) + Math.pow(initPosY - prevPosY, 2));
+        console.log(e.target.id, e.type);
 
         if (mouseDragged && dist > 5) {
             console.log('btn dragged');
+            e.preventDefault();
+            e.stopImmediatePropagation();
         } else {
             console.log('btn clicked');
-            toggleMuteState();
+            // toggleMuteState();
+            target.click();
         }
 
         // stop moving when mouse button is released:
-        // document.onmouseup = null;
-        // document.onmousemove = null;
         document.removeEventListener('mouseup', closeDragElement, true);
         document.removeEventListener('mousemove', elementDrag, true);
         mouseDragged = false;
+        clickIsHandledByDrag = false;
     }
 }
 
 function setMuteButtonContent(muted) {
-    bigMuteButton.innerHTML = muted ? 'volume_off' : 'volume_up';
+    bigMuteContent.innerHTML = muted ? 'volume_off' : 'volume_up';
 }
 
 async function toggleMuteState() {
     console.log('toggle mute state');
     const response = await chrome.runtime.sendMessage({bigMute: {toggleMute: true}});
-    bigMuteButton.innerHTML = response ? 'volume_off' : 'volume_up';
+    setMuteButtonContent(response);
 }
 
 async function getCurrentMuteState() {
